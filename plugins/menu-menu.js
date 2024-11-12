bc8c99b1dc900caeb6ee5cc0826b8655018d1618
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-11-11 14:10:20 -1100

    Create menu-menu.js

---
+++ plugins/menu-menu.js
@@ -0,0 +1,104 @@
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  try {
+    const pp = imagen1;
+    // let vn = './media/menu.mp3'
+    const img = './Menu2.jpg';
+    const d = new Date(new Date + 3600000);
+    const locale = 'es';
+    const week = d.toLocaleDateString(locale, {weekday: 'long'});
+    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = global.db.data.users[m.sender];
+    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const rtotal = Object.entries(global.db.data.users).length || '0'
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `┏━━━━• 𝐒𝐀𝐊𝐔 𝐀𝐈 •━━━━┓
+┃╭─────────♡─────────╮
+┃┊•👋🏻¡𝙃𝙊𝙇𝘼! @${m.sender.split`@`[0]}
+┃╰─────────♡─────────╯
+┃┊• 𝐂𝐑𝐄𝐀𝐃𝐎𝐑 👑
+┃┊• wa.me/524274130309
+┃┊• 𝙑𝙀𝙍𝙎𝙄𝙊𝙉 𝘿𝙀𝙇 𝘽𝙊𝙏 ⚙️
+┃┊• 2.0.0
+┃┊• 𝙁𝙀𝘾𝙃𝘼
+┃┊• ${date}
+┃┊• 𝙉𝙄𝙑𝙀𝙇
+┃┊• ${level}
+┃┊• 𝙀𝙓𝙋
+┃┊• ${exp} 
+┃┊• 𝙍𝘼𝙉𝙂𝙊
+┃┊• ${role} 
+┃┊• 𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀𝙎
+┃┊• ${limit}
+┃┊• 𝘾𝙊𝙄𝙉𝙎
+┃┊• ${money}
+┃┊• 𝙏𝙊𝙆𝙀𝙉𝙎
+┃┊• ${joincount}
+┃┊• 𝙏𝙄𝙀𝙈𝙋𝙊 𝘼𝘾𝙏
+┃┊• ${uptime}                                            
+┗━━━━━━━━•◦ 🤖 ◦•━━━━━━━━┛
+
+┏━━━━━━━━━━━━━━━━━━━━━┓
+┃「 ❄️𝙂𝙍𝙐𝙋𝙊𝙎❄️ 」
+┃╭───────⧟⭑⧟───────•
+┃┊❄️.Menu
+┃┊❄️.Tagall - .Todos
+┃┊❄️.Hidetag - .Notify
+┃┊❄️.Totag - .Tag
+┃╰───────⧟⭑⧟───────•
+┗━━━━━━━━━━━━━━━━━━━━━┛
+
+┏━━━━━━━━━━━━━━━━━━━━━┓
+┃「 🍭𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎🍭 」
+┃╭───────⧟⭑⧟───────•
+┃│🍭.𝘚𝘵𝘪𝘤𝘬𝘦𝘳 | 𝘴 𝘪𝘮𝘢𝘨𝘦𝘯 𝘰 𝘷𝘪𝘥𝘦𝘰
+┃╰───────⧟⭑⧟───────•
+┗━━━━━━━━━━━━━━━━━━━━━┛
+
+┏━━━━━━━━━━━━━━━━━━━━━┓
+┃「 🎮𝙅𝙐𝙀𝙂𝙊𝙎🎮 」
+┃╭───────⧟⭑⧟───────•
+┃│🎮.Minar
+┃│🎮.Work
+┃╰───────⧟⭑⧟───────•
+┗━━━━━━━━━━━━━━━━━━━━━┛
+
+┏━━━━━━━━━━━━━━━━━━━━━┓
+┃「 🈴𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙎🈴 」
+┃╭───────⧟⭑⧟───────•
+┃│🈴.𝘗𝘭𝘢𝘺
+┃│🈴.Ytmp4
+┃╰───────⧟⭑⧟───────•
+┗━━━━━━━━━━━━━━━━━━━━━┛`.trim();
+    if (m.isGroup) {
+    //conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m, null, rpro)
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, '🔮 𝙀𝙡 𝙢𝙚𝙣𝙪 𝙩𝙞𝙚𝙣𝙚 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧 𝙮 𝙣𝙤 𝙛𝙪𝙚 𝙥𝙤𝙨𝙞𝙗𝙡𝙚 𝙚𝙣𝙫𝙞𝙖𝙧𝙡𝙤 𝙥𝙤𝙧 𝙛𝙖𝙫𝙤𝙧 𝙧𝙚𝙥𝙤𝙧𝙩𝙚𝙡𝙤 𝙖𝙡 𝙘𝙧𝙚𝙖𝙙𝙤𝙧 𝙘𝙤𝙣 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤\n\n.𝘳𝘦𝘱𝘰𝘳𝘵𝘦 <𝘵𝘦𝘹𝘵𝘰>', m);
+  }
+};
+handler.command = /^(menu|menú)$/i;
+handler.exp = 50;
+handler.fail = null;
+export default handler;
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
+}
